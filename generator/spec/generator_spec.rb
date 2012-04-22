73292e1a0588acd04cf72e1771432c0dde6ef397
Ben Oakes <ben@benjaminoakes.com>
2012-04-22 11:26:02 -0400

    Start of specs

---
+++ generator/spec/generator_spec.rb
@@ -0,0 +1,7 @@
+require_relative './spec_helper'
+
+describe Generator do
+  it 'has specs' do
+    1.should == 1
+  end
+end
---
+++ generator/spec/spec_helper.rb
@@ -0,0 +1,8 @@
+require 'rubygems'
+require 'rspec'
+
+require_relative '../lib/generator'
+
+RSpec.configure do |c|
+  c.mock_with :rspec
+end
