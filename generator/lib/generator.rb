96af81fbf6593afe66c3c44ea5dfa2f40acd68ea
Ben Oakes <ben@benjaminoakes.com>
2012-04-22 12:10:45 -0400

    Add `Generator`
        Generator
          writes templated Markdown for its sessions to files

--- generator/lib/generator.rb
+++ generator/lib/generator.rb
@@ -1,4 +1,37 @@
+require 'rubygems'
+require 'mustache'
+
 require_relative './session_presenter'
 
 class Generator
+  def initialize(sessions)
+    @sessions = Array(sessions)
+  end
+
+  # Write all sessions to files.
+  def generate
+    @sessions.each do |s|
+      sp = SessionPresenter.new(s)
+      write(sp.filename, sp)
+    end
+  end
+
+  private
+
+  # Write a session to a file.
+  def write(filename, session)
+    # This is partially written using an `IO` because that makes testing easier (and `fakefs` isn't available to me because I'm working offline).
+    io = File.open(filename, 'w')
+    io.write(content(session))
+    io.close
+  end
+
+  # Template the session into Markdown.
+  def content(session)
+    Mustache.to_html(template, session)
+  end
+
+  def template
+    @template ||= File.read('Template.md.mustache')
+  end
 end
--- generator/spec/generator_spec.rb
+++ generator/spec/generator_spec.rb
@@ -1,7 +1,23 @@
+require 'stringio'
+
 require_relative './spec_helper'
 
 describe Generator do
-  it 'has specs' do
-    1.should == 1
+  before do
+    @data = [
+      {"title"=>"Rich Hickey", "abstract"=>"Rich Hickey, the author of <a href=\"http://clojure.org/\">Clojure</a> and designer of <a href=\"http://datomic.com/\">Datomic</a>", "name"=>"Rich Hickey", "bio"=>"Rich Hickey is a software developer with over 20 years of experience in various domains.", "starts_at"=>"2012-04-23T17:30:00Z", "ends_at"=>"2012-04-23T18:00:00Z", "category"=>"keynote", "room"=>"Salon HJK"},
+    ]
+
+    @generator = Generator.new(@data)
+  end
+
+  it 'writes templated Markdown for its sessions to files' do
+    io = StringIO.new
+    File.should_receive(:open).with('Rich-Hickey-Keynote.md', 'w').and_return(io)
+    @generator.generate
+    s = io.string
+    s.should match(/\*\*Presenter:\*\* Rich Hickey/)
+    s.should match(/> Rich Hickey is a software developer with over 20 years of experience in various domains/)
+    s.should match(%r{> Rich Hickey, the author of <a href="http://clojure.org/">Clojure</a> and designer of <a href="http://datomic.com/">Datomic</a>})
   end
 end
